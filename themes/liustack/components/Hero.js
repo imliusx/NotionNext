91462bdaa4cf81617a97c1ca035a4addc43731dc
imliusx <imliusx@163.com>
2025-03-25 23:34:45 +0800

    style: 放大首页标题和欢迎语字体 #标题从text-4xl5xl/6xl #欢迎语从text-lg2xl

--- themes/liustack/components/Hero.js
+++ themes/liustack/components/Hero.js
@@ -21,7 +21,8 @@ const Hero = props => {
     window.scrollTo({ top: wrapperTop, behavior: 'smooth' })
   }
 
-  const GREETING_WORDS = siteConfig('GREETING_WORDS').split(',')
+  // 欢迎语，改为用分号分割
+  const GREETING_WORDS = siteConfig('GREETING_WORDS').split(';')
   useEffect(() => {
     updateHeaderHeight()
 
@@ -62,11 +63,11 @@ const Hero = props => {
       className='w-full h-screen relative bg-black'>
       <div className='text-white absolute bottom-0 flex flex-col h-full items-center justify-center w-full '>
         {/* 站点标题 */}
-        <div className='font-black text-4xl md:text-5xl shadow-text'>
+        <div className='font-black text-5xl md:text-6xl shadow-text'>
           {siteInfo?.title || siteConfig('TITLE')}
         </div>
         {/* 站点欢迎语 */}
-        <div className='mt-2 h-12 items-center text-center font-medium shadow-text text-lg'>
+        <div className='mt-3 h-16 items-center text-center font-medium shadow-text text-2xl'>
           <span id='typed' />
         </div>
 
