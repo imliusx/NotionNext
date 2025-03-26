b639ea66022bd3d62c8944ae68bab2abc74a333e
imliusx <imliusx@163.com>
2025-03-26 15:13:52 +0800

    修复小屏幕状态下Card组件的边距过近问题

--- themes/liustack/components/Card.js
+++ themes/liustack/components/Card.js
@@ -1,6 +1,6 @@
 const Card = ({ children, headerSlot, className }) => {
   // 调整小屏幕状态下的卡片边距
-  return <div className={`{className} mt-4 md:mt-0 px-2`}>
+  return <div className={`${className} mt-6 md:mt-0 px-2`}>
     <>{headerSlot}</>
     <section className="card shadow-md hover:shadow-md dark:text-gray-300 border dark:border-black rounded-xl lg:p-6 p-4 bg-white dark:bg-hexo-black-gray lg:duration-100">
         {children}
