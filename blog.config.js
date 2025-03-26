f33ad493b772a571e993566f1245a1171030cd02
imliusx <imliusx@163.com>
2025-03-26 17:27:13 +0800

    欢迎语

--- blog.config.js
+++ blog.config.js
@@ -58,7 +58,7 @@ const BLOG = {
   LAYOUT_SIDEBAR_REVERSE: process.env.NEXT_PUBLIC_LAYOUT_SIDEBAR_REVERSE || true,
 
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
-  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'Stack the Code，Hack the World.',
+  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'Stack the Code, Hack the World.',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false,
