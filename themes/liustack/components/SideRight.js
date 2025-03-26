2387a3fb6b479f6d795930cb38de9753196df984
imliusx <imliusx@163.com>
2025-03-26 16:58:14 +0800

    边距

--- themes/liustack/components/SideRight.js
+++ themes/liustack/components/SideRight.js
@@ -56,7 +56,7 @@ export default function SideRight(props) {
   return (
     <div
       id='sideRight'
-      className={` lg:w-80 lg:pt-2 ${post ? 'lg:pt-0' : 'lg:pt-4'} mt-6 md:mt-0 px-2`}>
+      className={` lg:w-80 lg:pt-2 ${post ? 'lg:pt-0' : 'lg:pt-4'} mt-6 md:mt-0 px-4`}>
       <div className='sticky top-8 space-y-4'>
         {post && post.toc && post.toc.length > 1 && (
           <Card>
