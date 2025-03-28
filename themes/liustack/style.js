a05475d491e76b222f4fe5f72599c7603cd8767a
imliusx <imliusx@163.com>
2025-03-29 02:02:38 +0800

    fix

--- themes/liustack/style.js
+++ themes/liustack/style.js
@@ -81,7 +81,19 @@ const Style = () => {
     /* 代码字体 */
     pre, code {
         font-family: "JetBrains Mono";
-    } 
+    }
+
+    .notion {
+      /* 正文内容 */
+      .notion-text {
+        font-size: 15px;
+      }
+      
+      /* 标题 */
+      .notion-h1 { font-size: 2.2rem; }
+      .notion-h2 { font-size: 1.8rem; }
+      .notion-h3 { font-size: 1.5rem; }
+    }
   `}</style>)
 }
 
