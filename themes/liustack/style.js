75eb05a5058f14874f1ac144960bc25cf0bcb9aa
imliusx <imliusx@163.com>
2025-03-28 17:28:31 +0800

    调整正文文字大小

--- themes/liustack/style.js
+++ themes/liustack/style.js
@@ -93,14 +93,8 @@ const Style = () => {
     .notion {
       /* 正文内容 */
       .notion-text {
-        font-size: 20px;
-        line-height: 1.8;
+        font-size: 18px;
       }
-      
-      /* 标题 */
-      .notion-h1 { font-size: 2.2rem; }
-      .notion-h2 { font-size: 1.8rem; }
-      .notion-h3 { font-size: 1.5rem; }
     }
       
   `}</style>)
