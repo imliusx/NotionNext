5fe631920f6836f237e0d7532b2bedf322760671
imliusx <imliusx@163.com>
2025-04-01 13:23:35 +0800

    rename

--- blog.config.js
+++ blog.config.js
@@ -13,8 +13,8 @@ const BLOG = {
   APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'dark', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Ryen Liu', // 您的昵称 
-  BIO: process.env.NEXT_PUBLIC_BIO || '𝑺𝒕𝒂𝒄𝒌 𝒕𝒉𝒆 𝑪𝒐𝒅𝒆, 𝑯𝒂𝒄𝒌 𝒕𝒉𝒆 𝑾𝒐𝒓𝒍𝒅.', // 作者简介
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Ryan Liu', // 您的昵称 
+  BIO: process.env.NEXT_PUBLIC_BIO || 'Stack the Code, Hack the World. ', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://liusx.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '技术,生活', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
